9119a5d249a02db180635c98ffb8f22d79a97d6d
George Evans <georgeevans1995@gmail.com>
2020-10-08 14:37:55 +0100

    fix default theme

--- packages/frontend/react-components/src/types/styled-components.d.ts
+++ packages/frontend/react-components/src/types/styled-components.d.ts
@@ -4,6 +4,6 @@ import { ThemeType } from "../theme-provider/ThemeProvider";
 
 // and extend them!
 declare module "styled-components" {
-
-  export type DefaultTheme = ThemeType;
+  // eslint-disable-next-line @typescript-eslint/no-empty-interface
+  export interface DefaultTheme extends ThemeType {}
 }
