1d8f0008a2aeaf382ca95050fff35bc4ff90d9d8
Julian Regan <jregan@Julians-iMac.local>
2020-12-10 11:29:21 +0000

    Allow passing callback to useLocalStorage setValue function to avoid infinite loops in useEffect

--- packages/frontend/react-hooks/src/useLocalStorage.ts
+++ packages/frontend/react-hooks/src/useLocalStorage.ts
@@ -3,12 +3,13 @@ import { createDebug } from "@gravitywelluk/debug";
 
 const logger = createDebug("LOCAL_STORAGE");
 
+type SetLocalStorageValue<T> = (valueOrCallback: React.SetStateAction<T>) => void;
 /**
  * Use local storage hook
  */
 export const useLocalStorage = <T = Record<string, unknown>>(key: string, initialValue: T): [
   T,
-  (value: T) => void
+  SetLocalStorageValue<T>
 ] => {
   // State to store our value
   // Pass initial state function to useState so logic is only executed once
@@ -29,12 +30,31 @@ export const useLocalStorage = <T = Record<string, unknown>>(key: string, initia
 
   // Return a wrapped version of useState's setter function that persists
   // the new value to localStorage.
-  const setValue = React.useCallback((value: T) => {
+  // can pass a callback to avoid useEffect loops if updating an existing value
+  const setValue = React.useCallback<SetLocalStorageValue<T>>(valueOrCallback => {
     try {
-      // Save state
-      setStoredValue(value);
-      // Save to local storage
-      window.localStorage.setItem(key, JSON.stringify(value));
+      // standard use case (object)
+      if (typeof valueOrCallback === "object") {
+        const value = valueOrCallback;
+
+        // Save state
+        setStoredValue(value);
+        // Save to local storage
+        window.localStorage.setItem(key, JSON.stringify(value));
+
+      // if a callback is passed
+      } else {
+        // type discrimination doesn't work properly here so need to cast
+        const updateStoredValueCallback = valueOrCallback as (prevState: T) => T;
+
+        setStoredValue(currentValue => {
+          const updatedValue = updateStoredValueCallback(currentValue);
+
+          window.localStorage.setItem(key, JSON.stringify(updatedValue));
+
+          return updatedValue;
+        });
+      }
     } catch (error) {
       // A more advanced implementation would handle the error case
       logger.error(error);
