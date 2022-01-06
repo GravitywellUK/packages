fc10024f6ac9d4fed959be25698d8ee8033ce315
Julian Regan <julianregan@me.com>
2022-01-06 10:51:16 +0000

    Add sorting + filtering, fix useEffect loops, improve docs

--- packages/frontend/react-hooks/src/usePagination.ts
+++ packages/frontend/react-hooks/src/usePagination.ts
@@ -1,46 +1,51 @@
 import {
-  useMemo,
-  useReducer
+  useReducer,
+  useRef
 } from "react";
 
-export type SortOrder = "ASC" | "DESC";
+export type SortOrder<T> = Array<[ keyof T, "ASC" | "DESC" ]>;
 
-export interface PaginationState {
+export interface PaginationState<ResponseObject, FilterOptions> {
   /** Starting item index of the current page */
   offset: number;
   /** Page size */
   limit: number;
-  /** Sort order */
-  order: SortOrder;
+  /** Results sorting */
+  order?: SortOrder<ResponseObject>;
+  /** Results filtering */
+  filters?: FilterOptions;
 }
 
-export interface PaginationActions {
+export interface PaginationActions<ResponseObject, FilterOptions> {
   /** Load the next page */
   next: () => void;
   /** Load the previous page */
   previous: () => void;
-  /** Set sort order */
-  sort: (order: SortOrder) => void;
-  /** Reverse sort order */
-  invert: () => void;
   /** Set page size */
   setPageSize: (pageSize: number) => void;
+  /** Set sort key and order */
+  sort: (order: SortOrder<ResponseObject> | null) => void;
+  /** Set filters */
+  filter: (filters: FilterOptions | null) => void;
   /** Reset to initial pagination state */
   reset: () => void;
 }
 
-// Full pagination object, returned by the usePagination hook
-export interface Pagination extends PaginationState, PaginationActions {
-  /** Human-readable page number */
-  currentPage: number;
+export interface Pagination<ResponseObject, FilterOptions> {
+  /** Pagination params for controlling a paginated API endpoint */
+  params: PaginationState<ResponseObject, FilterOptions>;
+  /** Pagination actions for updating pagination state */
+  actions: PaginationActions<ResponseObject, FilterOptions>;
 }
 
-interface ReducerAction {
-  type: keyof PaginationActions;
-  payload?: Partial<PaginationState>;
+interface ReducerAction<ResponseObject, FilterOptions> {
+  type: keyof PaginationActions<ResponseObject, FilterOptions>;
+  payload?: Partial<PaginationState<ResponseObject, FilterOptions>>;
 }
 
-const reducer = (state: PaginationState, action: ReducerAction): PaginationState => {
+type PaginationReducer<T, U> = React.Reducer<PaginationState<T, U>, ReducerAction<T, U>>;
+
+const reducer = <T, U>(state: PaginationState<T, U>, action: ReducerAction<T, U>): PaginationState<T, U> => {
   if (action.type === "next") {
     return {
       ...state,
@@ -53,65 +58,60 @@ const reducer = (state: PaginationState, action: ReducerAction): PaginationState
       ...state,
       offset: canGoBack ? state.offset - state.limit : state.offset
     };
-  } else if (action.type === "sort" && action.payload?.order) {
-    return {
-      ...state,
-      offset: 0,
-      order: action.payload.order
-    };
-  } else if (action.type === "invert") {
-    return {
-      ...state,
-      offset: 0,
-      order: state.order === "ASC" ? "DESC" : "ASC"
-    };
   } else if (action.type === "setPageSize" && action.payload?.limit) {
     return {
       ...state,
       offset: 0,
       limit: action.payload.limit
     };
-  } else if (action.type === "reset" && action.payload) {
+  } else if (action.type === "sort") {
     return {
       ...state,
-      ...action.payload
+      offset: 0,
+      order: action.payload?.order
     };
+  } else if (action.type === "filter") {
+    return {
+      ...state,
+      offset: 0,
+      filters: action.payload?.filters
+    };
+  } else if (action.type === "reset" && action.payload) {
+    return action.payload as PaginationState<T, U>;
   }
 
   return state;
 };
 
-type UsePagination = (initialState: PaginationState) => Pagination;
-
 /** Custom reducer to handle offset-based pagination of API endpoints in React */
-export const usePagination: UsePagination = initialState => {
-  const [ paginationState, dispatch ] = useReducer(reducer, initialState);
+// eslint-disable-next-line @typescript-eslint/ban-types
+export const usePagination = <ResponseObject extends object, FilterOptions extends object = Partial<ResponseObject>>(initialState: PaginationState<ResponseObject, FilterOptions>): Pagination<ResponseObject, FilterOptions> => {
+  const [ paginationState, dispatch ] = useReducer<PaginationReducer<ResponseObject, FilterOptions>>(reducer, initialState);
 
-  const currentPage = useMemo(() => {
-    return (paginationState.offset / paginationState.limit) + 1;
-  }, [ paginationState ]);
-
-  const paginationActions = useMemo<PaginationActions>(() => ({
+  // useRef ensures that paginationActions remains stable, avoiding useEffect loops when calling these actions
+  const paginationActions = useRef<PaginationActions<ResponseObject, FilterOptions>>({
     next: () => dispatch({ type: "next" }),
     previous: () => dispatch({ type: "previous" }),
-    sort: (order: SortOrder) => dispatch({
-      type: "sort",
-      payload: { order }
-    }),
-    invert: () => dispatch({ type: "invert" }),
-    setPageSize: (pageSize: number) => dispatch({
+    setPageSize: pageSize => dispatch({
       type: "setPageSize",
       payload: { limit: pageSize }
+    }),
+    sort: order => dispatch({
+      type: "sort",
+      payload: { order: order || undefined }
+    }),
+    filter: filters => dispatch({
+      type: "filter",
+      payload: { filters: filters || undefined }
     }),
     reset: () => dispatch({
       type: "reset",
       payload: initialState
     })
-  }), [ initialState ]);
+  });
 
   return {
-    currentPage,
-    ...paginationState,
-    ...paginationActions
+    params: paginationState,
+    actions: paginationActions.current
   };
 };
